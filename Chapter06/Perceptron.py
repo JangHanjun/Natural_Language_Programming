227d35584b618bc194055b05fd711c980921410f
husk <huskgit@gmail.com>
2022-02-16 15:42:20 +0900

    단층 퍼셉트론 실습

---
+++ Chapter06/Perceptron.py
@@ -0,0 +1,26 @@
+def AND_gate(x1, x2):
+    w1 = 0.5
+    w2 = 0.5
+    b = -0.7
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
+        return 0
+    return 1
+
+def NAND_gate(x1, x2):
+    w1 = -0.5
+    w2 = -0.5
+    b = 0.7
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
+        return 1
+    return 0
+
+def OR_gate(x1, x2):
+    w1 = 0.6
+    w2 = 0.6
+    b = 0.5
+    result = x1*w1 + x2*w2 + b
+    if result <= 0:
+        return 0
+    return 
